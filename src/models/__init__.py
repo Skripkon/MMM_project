fc66a37cc7c06da34c12eff43ff1d8c1d069a2c8
setday <32331578+setday@users.noreply.github.com>
2025-12-04 03:44:35 +0300

    Add models to __init__

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,5 +2,7 @@ from src.models.mlp_baseline import MLPBaselineModel
 from src.models.cube_baseline import CubeBaselineModel
 from src.models.clip_lstm_model import CLIPLSTMModel
 from src.models.clip_lstm_model_v2 import CLIPLSTMModelV2
+from src.models.cool_model import MultiModalFusionModel as CoolModel
+from src.models.resnet_dual_path import MultiModalFusionModel as ResNetDualPathModel
 
-__all__ = ["MLPBaselineModel", "CubeBaselineModel", "CLIPLSTMModel", "CLIPLSTMModelV2"]
+__all__ = ["MLPBaselineModel", "CubeBaselineModel", "CLIPLSTMModel", "CLIPLSTMModelV2", "CoolModel", "ResNetDualPathModel"]
